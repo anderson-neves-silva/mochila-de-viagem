61b99d88c110cd7f224ea72d72e8612ff9c832af
Anderson Neves <andersonneves_@hotmail.com>
2022-10-07 10:08:19 -0300

    copying user input data

--- js/main.js
+++ js/main.js
@@ -1,79 +1,18 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach( (elemento) => {
-    criaElemento(elemento)
-} )
-
+//capiturando o formulário pelo seu id e salvando em uma variável
+const form = document.getElementById("novoItem");
+/*
+operação de um furmulario ser enviado e a de submit, uso aqui um escutador de evento que nesse caso e o submit, e quando 
+acontecer esse evento ele irá executar algo para mim, aqui ao dicitar os dados no input do form e clico nada acontece, isso porque
+devemos interromper o comportamento padrão, e fazemos isso com o "preventDefault", note que passamos o evento como parâmetro nessa
+função, aqui precisamos pegar os dados de entrada nome e quantidade e tem diversas formas de fazer isso mas aqui vamos pegar pelo 
+alvo também conhecido como "target" e em "elements" pois aqui é um objeto   
+*/
 form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+    evento.preventDefault();
+    console.log(evento);
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
-
-    const existe = itens.find( elemento => elemento.nome === nome.value )
-
-    const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value
-    }
-
-    if (existe) {
-        itemAtual.id = existe.id
-        
-        atualizaElemento(itemAtual)
-
-        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
-    } else {
-        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
-
-        criaElemento(itemAtual)
-
-        itens.push(itemAtual)
-    }
-
-    localStorage.setItem("itens", JSON.stringify(itens))
-
-    nome.value = ""
-    quantidade.value = ""
-})
-
-function criaElemento(item) {
-    const novoItem = document.createElement("li")
-    novoItem.classList.add("item")
-
-    const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = item.quantidade
-    numeroItem.dataset.id = item.id
-    novoItem.appendChild(numeroItem)
-    
-    novoItem.innerHTML += item.nome
-
-    novoItem.appendChild(botaoDeleta(item.id))
-
-    lista.appendChild(novoItem)
-}
-
-function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
-}
-
-function botaoDeleta(id) {
-    const elementoBotao = document.createElement("button")
-    elementoBotao.innerText = "X"
-
-    elementoBotao.addEventListener("click", function() {
-        deletaElemento(this.parentNode, id)
-    })
-
-    return elementoBotao
-}
-
-function deletaElemento(tag, id) {
-    tag.remove()
-
-    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
-
-    localStorage.setItem("itens", JSON.stringify(itens))
-}
+    evento.target[0].value;
+    console.log(evento.target.elements['nome'].value);
+    console.log(evento.target.elements['quantidade'].value);
+});
